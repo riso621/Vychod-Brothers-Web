09dd986bc500bb15e8417497843dc65415d57481
Richard Vanci <voreno.web@gmail.com>
2026-08-05 14:24:02 +0200

    Shorten player controls timeout to three seconds

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -73,7 +73,7 @@ function CloudflareTrackedPlayer({ playerUrl, title, videoId, watchProgress, onW
     return url.toString()
   }, [playerUrl])
 
-  const revealControls = useCallback((delay = 5000) => {
+  const revealControls = useCallback((delay = 3000) => {
     setControlsVisible(true)
     setControlsHideDelay(delay)
     setControlsActivity((value) => value + 1)
