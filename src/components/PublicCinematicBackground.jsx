f1a8bea7ecb0f7145b0cff929005cc9f30316af9
Richard Vanci <voreno.web@gmail.com>
2026-08-19 00:58:08 +0200

    feat: enrich public cinematic background

--- src/components/PublicCinematicBackground.jsx
+++ src/components/PublicCinematicBackground.jsx
@@ -2,8 +2,11 @@ export default function PublicCinematicBackground() {
   return <div className="public-cinematic-background" aria-hidden="true">
     <span className="public-cinematic-haze is-one" />
     <span className="public-cinematic-haze is-two" />
+    <span className="public-cinematic-veins is-left" />
+    <span className="public-cinematic-veins is-right" />
     <span className="public-cinematic-beams" />
     <span className="public-cinematic-particles" />
+    <span className="public-cinematic-embers" />
     <span className="public-cinematic-pulse" />
     <span className="public-cinematic-grain" />
   </div>
